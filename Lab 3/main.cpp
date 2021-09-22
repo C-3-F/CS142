631b250bd2bf3b198a614b5d2b64af9d2723a912
Cannon Farr <c3farr@gmail.com>
2021-09-22 11:11:21 -0600

    initial commit

---
+++ Lab 3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int sumValues(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    cout << sumValues(50, 38, 2880) << endl;
+    return 0;
+}
